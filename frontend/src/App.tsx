66a459acd2a7ff5abe03bbca5e195fdd38b3197a
mmcampbell04 <meghan@cygnet.io>
2025-10-28 17:53:18 +0000

    reorganise pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,32 @@
 import { Route, Routes } from "react-router-dom";
+import { ConnectRavelry } from "./components/ConnectRavelry";
 import DashboardLayout from "./components/DashboardLayout";
 import { Home } from "./components/Home";
 import Layout from "./components/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useCurrentUser } from "./lib/hooks/useAuth";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
+  const { data: user, isLoading } = useCurrentUser();
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              isLoading ? (
+                <p>Loading...</p>
+              ) : user?.id ? (
+                <DashboardLayout />
+              ) : (
+                <Home />
+              )
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="dashboard">
@@ -22,10 +37,10 @@ function App() {
                 </ProtectedRoute>
               }
             >
-              <Route index element={<div>Library</div>} />
-              <Route path="library" element={<div>Library</div>} />
+              <Route index path="library" element={<div>Library</div>} />
               <Route path="projects" element={<div>Projects</div>} />
               <Route path="tools" element={<div>Tools</div>} />
+              <Route path="connect-ravelry" element={<ConnectRavelry />} />
             </Route>
           </Route>
         </Route>
